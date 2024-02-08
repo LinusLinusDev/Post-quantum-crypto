422b97104d84f888b113ab6d13b36b2d3279afe0
Linus Palm <89220256+LinusLinusDev@users.noreply.github.com>
2024-02-08 16:40:52 +0100

    lamport update

--- hash_based.py
+++ hash_based.py
@@ -1,31 +1,22 @@
 from hashlib import sha256
 import random
 
-# different random values in secret key for seed = -1, same values in secret key for seed > -1
-seed = -1
-
-if seed >= 0:
-    random.seed(seed)
+seed = 0
+random.seed(seed)
 
 
 class Lamport:
     # hash = cryptographic hash function used for g and f
     # n = security parameter set to the digest size of the hash function in bits
     # K = maximum decimal number that can be represented with n bits
-    # n = number of total variables
-    # S = affine map
-    # private = quadratic map with trapdoor
-    def __init__(self, hash):
+    # private = private key
+    # public = public key
+    def __init__(self, hash, n):
         self.__hash = hash
-        self.__n = self.__hash("".encode("UTF-8")).digest_size * 8
+        self.__n = n
         self.__K = pow(2, self.__n) - 1
         self.__private = self.generate_private_key()
         self.__public = self.generate_public_key()
-
-    # setup OWF function {0,1}^n -> {0,1}^n
-    def g(self, value):
-        value_hash = self.__hash(str(value).encode('UTF-8')).hexdigest()
-        return int(value_hash, 16)
 
     # setup cryptographic hash function {0,1}^* -> {0,1}^n
     def f(self, message):
@@ -34,14 +25,14 @@ class Lamport:
         # cast hash to binary representation
         h_bin = bin(int(h, 16))[2:]
         # add missing zeros at the beginning of the binary representation
-        if int(h[0], 16) < 2:
-            h_bin = '000' + h_bin
-        elif int(h[0], 16) < 4:
-            h_bin = '00' + h_bin
-        elif int(h[0], 16) < 8:
-            h_bin = '0' + h_bin
+        length = self.__hash("".encode("UTF-8")).digest_size * 8
+        h_bin = h_bin.zfill(length)
 
-        return h_bin
+        return h_bin[:self.__n]
+
+    # setup OWF function
+    def g(self, value):
+        return int(self.f(str(value)), 2)
 
     # generate private key
     def generate_private_key(self):
@@ -54,6 +45,9 @@ class Lamport:
             y_tuple = (self.g(x_tuple[0]), self.g(x_tuple[1]))
             y.append(y_tuple)
         return y
+
+    def get_private(self):
+        return self.__private
 
     def get_public(self):
         return self.__public
@@ -84,9 +78,11 @@ class Lamport:
         return verify
 
 
-X = Lamport(sha256)
+X = Lamport(sha256, 16)
 document = "Hello World!"
 
+print(f"Private key: {X.get_private()}")
+print()
 print(f"Public key: {X.get_public()}")
 print()
 print(f"Document: {document}")
